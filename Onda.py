ade0d9aa2a34a5bd82ae2b65ee1919eba69f4785
Diego <darkar315@darkar315-virtual-machine>
2017-07-17 17:47:56 -0500

    Falta animacion

--- Onda.py
+++ Onda.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 Np = 300
 a = 0
@@ -10,14 +11,19 @@ dxy = xy[1] - xy[0]
 
 Nt1 = 60
 Nt2 = 30
-dt = 0.5 * dxy
 
+r = 0.5
+dt = r * dxy
 c = 1.
 r_0 = ((c**2) * dt) / (2. * dxy)
-r = 0.5
+
+barrera = np.ones((Np, Np))
+barrera[200] = 0
+barrera[200][135 : 165] = 1.
 
 u_0 = np.zeros((Np, Np))
-u_0[150][100] = -0.5
+u_0[100][150] = -0.5
+u_0 = u_0 * barrera
 
 u_fut = np.zeros((Np, Np))
 
@@ -30,22 +36,41 @@ def it_tiempo(N):
             u_fut[i][j] = u_0[i][j] + (r_0 * (u_0[i + 1][j] - u_0[i - 1][j] + u_0[i][j + 1] - u_0[i][j - 1]))
 
     u_past = u_0.copy()
-    u_pres = u_fut.copy()
+    u_pres = u_fut.copy() * barrera
     lis.append(u_pres)
 
-    for t in range(N):
+    for t in range(1, int(N / dt)):
         for i in range(1, Np - 1):
             for j in range(1, Np - 1):
                 u_fut[i][j] = ((r**2) * (u_pres[i + 1][j] + u_pres[i - 1][j] + u_pres[i][j + 1] + u_pres[i][j - 1])) + (2. * (1. - (2. * (r**2))) * u_pres[i][j]) - u_past[i][j]
         u_past = u_pres.copy()
-        u_pres = u_fut.copy()
+        u_pres = u_fut.copy() * barrera
         lis.append(u_pres)
 
     return lis
 
 t_60 = it_tiempo(Nt1)
-mat_30 = t_60[Nt2]
-mat_60 = t_60[Nt1]
+mat_30 = t_60[598]
+mat_60 = t_60[1196]
 
-print mat_30[150][100]
-print mat_60[150][100]
+plt.imshow(mat_30, cmap = 'gray')
+plt.title('Onda en t = 30s')
+plt.savefig('Ondas_30.png')
+plt.close()
+
+plt.imshow(mat_60, cmap = 'gray')
+plt.title('Onda en t = 60s')
+plt.savefig('Ondas_60.png')
+plt.close()
+
+contenedor = plt.imshow(t_60[0], cmap = 'gray')
+
+fig = plt.figure()
+
+def func(i):
+    datos = t_60[i]
+    contenedor.set_array(datos)
+    return contenedor
+
+animation.FuncAnimation(fig, func)
+plt.show()
