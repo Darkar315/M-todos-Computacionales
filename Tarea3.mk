c9f462ca1d6f746ffab6d87aa675cd41c34ff689
Diego <darkar315@darkar315-virtual-machine>
2017-07-20 21:48:33 -0500

    Makefile final

--- Tarea3.mk
+++ Tarea3.mk
@@ -1,21 +1,18 @@
 #tengo un pdf con todas las imagenes que se llama Resultados_hw3.pdf
 
-Resultados_hw3.pdf : Resultados_hw3.tex  Ondas.pdf Orbitas.pdf
+Resultados_hw3.pdf : Resultados_hw3.tex Ondas_30.pdf Orbitas.pdf
 	pdflatex Resultados_hw3.tex
 
-Ondas.pdf : Onda.py
+Ondas_30.pdf : Onda.py
 	python Onda.py 
 
 Orbitas.pdf : Plots_Planetas.py
-	python  Plots_Planetas.py
+	python Plots_Planetas.py
 
 
 Plots_Planetas.py : datos.txt Planetas.x
 	./Planetas.x > datos.txt 
 
 Planetas.x : Planetas.c
-	cc -lm Planetas.c -o Planetas.x
-
- 
-
+	cc Planetas.c -o Planetas.x -lm
 
